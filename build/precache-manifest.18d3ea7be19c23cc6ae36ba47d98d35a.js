ca5e822984700e86f965cf0f762390120f2ca79a
harry <hachaku999@gmail.com>
2020-12-17 10:26:52 +0530

    mettle21_1

---
+++ build/precache-manifest.18d3ea7be19c23cc6ae36ba47d98d35a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70472180858d54fc498c96e1262e656d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb8df9958330f4caea31",
+    "url": "/static/css/main.9ca6130d.chunk.css"
+  },
+  {
+    "revision": "d562f85db8c4f9a54f00",
+    "url": "/static/js/2.d37f0c6a.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/static/js/2.d37f0c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8df9958330f4caea31",
+    "url": "/static/js/main.2d5216b3.chunk.js"
+  },
+  {
+    "revision": "e40e80a7da7b6fe4cccc",
+    "url": "/static/js/runtime-main.13cf9406.js"
+  },
+  {
+    "revision": "f68a0debec1abda2a35d990e5140d001",
+    "url": "/static/media/Blog1.f68a0deb.jpeg"
+  },
+  {
+    "revision": "3bdbe2602665a73ae5c3c9af7e558658",
+    "url": "/static/media/Blog2.3bdbe260.jpg"
+  },
+  {
+    "revision": "790b16ac2349349d73bfca23154fc749",
+    "url": "/static/media/Blog3.790b16ac.png"
+  },
+  {
+    "revision": "30815a2b7b8a19408aff7866786ed2a0",
+    "url": "/static/media/banner.30815a2b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "48b2f5b3536db415f7f2a740e52b0904",
+    "url": "/static/media/blog4.48b2f5b3.jpg"
+  },
+  {
+    "revision": "2144831d911da5f9a0394acda3aa49b3",
+    "url": "/static/media/case study.2144831d.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "a6d72fd0ebbf598b2189b68f8e40c575",
+    "url": "/static/media/events.a6d72fd0.jpg"
+  },
+  {
+    "revision": "ab6949b7bcaebb517dedfd8b30b9be20",
+    "url": "/static/media/landing.ab6949b7.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/mariya-georgieva.f1d71f77.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "9ca1f733f45a1aa7c0e1657275df5d84",
+    "url": "/static/media/pf1.9ca1f733.jpg"
+  },
+  {
+    "revision": "784c023f20e0e5b01297373a58884bb7",
+    "url": "/static/media/prof1f.784c023f.jpg"
+  },
+  {
+    "revision": "ca683780904ea92fb4df7563a2b31bfd",
+    "url": "/static/media/prof2.ca683780.jpg"
+  },
+  {
+    "revision": "e81697437f997e104fb277286ad06e5a",
+    "url": "/static/media/prof4.e8169743.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "e69f9746b422df0cba7cf10291a6a7c6",
+    "url": "/static/media/profile.e69f9746.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/studio-1.e0508cc9.jpg"
+  },
+  {
+    "revision": "6d02b89dad2c011dff602608082866f3",
+    "url": "/static/media/studio-2.6d02b89d.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "4c56e495fd59e14d855af7433d00302f",
+    "url": "/static/media/workshops.4c56e495.jpg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3214;
+
+app.use(express.static(path.join(__dirname,'build')));
+
+app.get('/',(req,res)=>{
+    
+    res.sendFile(path.join(__dirname,'build/index.html'))
+});
+
+app.listen(port,(err)=>{
+    if(err)throw err;
+    console.log('server is running harry')
+});
